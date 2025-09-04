52b49c9e3ef3ff241a0d0196655662ee119f5842
pedrobenedetmachado <pedrohbm0102@gmail.com>
2025-09-04 18:33:55 -0300

    medida de metros

---
+++ ex008.py
@@ -0,0 +1,8 @@
+from statistics import median_low
+
+medida =float(input('distancia em metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('a medida de {}m corresponde a {}cm'.format(medida, cm))
+print('e a medida de {}m corresponde a {}mm'.format(medida, mm))
+
